e2839a3a381236eb6d5787f59ca6d4ae458147d2
Arthur Sir. <35264395+CrocoDeals@users.noreply.github.com>
2025-05-31 23:13:59 +0200

    1

---
+++ Assets/Level Design Prefabs/Scripts/UVScrolling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScrolling : MonoBehaviour
+{
+    public float scrollX = 0.5f;
+    public float scrollY = 0.5f;
+    public int materialIndex = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offsetX = Time.time * scrollX;
+        float offsetY = Time.time * scrollY;
+
+        //GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+        GetComponent<Renderer>().materials[materialIndex].mainTextureOffset = new Vector2(offsetX, offsetY);
+        
+    }
+}
